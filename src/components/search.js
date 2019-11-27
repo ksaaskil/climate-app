e0c8f4b946bb1181eba5b202ce6fe4e8d4b353a6
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-11-27 15:45:20 +0200

    Add search.

--- src/components/search.js
+++ src/components/search.js
@@ -8,7 +8,7 @@ const Search = () => {
   }
   return (
     <div className="search">
-      <div className="search__title">Search a product</div>
+      <div className="search__title">Search product</div>
       <div className="search__input">
         <input type="text" value={value} onChange={handleChange} />
         <div></div>
