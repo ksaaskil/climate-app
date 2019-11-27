09793e229e1a6e2958520a7d1c0234091900770a
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-11-27 15:43:00 +0200

    Work on search and convert.

--- src/components/convert.js
+++ src/components/convert.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import "./convert.css"
 
 const Convert = () => {
+  const [value, setValue] = useState("")
+  const handleChange = event => {
+    setValue(event.target.value)
+  }
   return (
     <div className="convert">
       <div className="convert__title">Convert units</div>
+      <div className="convert__input">
+        <input type="text" value={value} onChange={handleChange} />
+        <div>kg CO_2</div>
+      </div>
+      {value ? (
+        <div className="convert__result">
+          <div className="convert__result--item convert__result-average">
+            {parseFloat(value) / 1000} km driving with a new car
+          </div>
+        </div>
+      ) : null}
     </div>
   )
 }
--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import "./search.css"
 
 const Search = () => {
+  const [value, setValue] = useState("")
+  const handleChange = event => {
+    setValue(event.target.value)
+  }
   return (
     <div className="search">
       <div className="search__title">Search a product</div>
+      <div className="search__input">
+        <input type="text" value={value} onChange={handleChange} />
+        <div></div>
+      </div>
     </div>
   )
 }
